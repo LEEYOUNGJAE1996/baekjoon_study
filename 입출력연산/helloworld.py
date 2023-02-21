46072c93cc8eaa2d925027475b149ad2251991b3
YOUNG <youngjae9683@gmail.com>
2023-02-22 01:08:44 +0900

    백준 입출력과 사칙연산

---
+++ 입출력연산/helloworld.py
@@ -0,0 +1,40 @@
+# Hello World
+print("Hello World!")
+
+# A+B
+# 왜 꼭 스플릿으로 해아하는 건가... 조건 중에 두개의 수를 한 번에 입력 받아야 된다는 말이
+# 있어야 하는거 아닌가??
+A, B = input().split()
+A = int(A)
+B = int(B)
+print(A+B)
+
+# A-B
+A, B = input().split()
+A = int(A)
+B = int(B)
+print(A-B)
+
+# 꼬마 정민
+A, B, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+print(A+B+C)
+
+# 고양이 그리기
+print('''
+\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|
+    ''')
+
+# 개 그리기
+print('''
+|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\\\\__|
+''')
