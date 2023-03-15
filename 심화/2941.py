ebb0fe023b52f9c59b7da7dc60aebf4d1cec39d6
YOUNG <youngjae9683@gmail.com>
2023-03-15 11:16:18 +0900

    230315 파이썬의 편리함, count 함수

---
+++ 심화/2941.py
@@ -0,0 +1,18 @@
+# 230315
+
+# 크로아티아 알파벳
+
+
+counting = 0
+strings = input()
+
+counting += strings.count('c=')
+counting += strings.count('c-')
+counting += strings.count('dz=')
+counting += strings.count('d-')
+counting += strings.count('lj')
+counting += strings.count('nj')
+counting += strings.count('s=')
+counting += strings.count('z=')
+
+print(len(strings)-counting)
